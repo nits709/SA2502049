c47a6ec76beec8930f6454b40bfa977f44110c22
nitin <nitingupta3005@gmail.com>
2025-06-11 09:29:34 +0530

    10 June 2025

--- src/test/java/authenticationType/BearerTokenAuthentication.java
+++ src/test/java/authenticationType/BearerTokenAuthentication.java
@@ -10,12 +10,12 @@ import io.restassured.response.Response;
 public class BearerTokenAuthentication {
 
 	
-	//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
+	
 	
 	
 	@Test
 	public void bearerTokenTest() {
-		String btoken = "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";
+		String btoken = "Replace Your Github person Access Token";
 		
 		
 		Response res = given().header("Authorization","Bearer "+btoken)
